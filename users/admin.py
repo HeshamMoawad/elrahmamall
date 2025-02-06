c81d6b97df7ef5a7f5a305f08efa4386aa966516
Hesham Moawad <heshammoawad120120@yahoo.com>
2025-02-06 16:25:04 +0200

    :zap: Add admin interface and custom user manager for ElRahmaUser

---
+++ users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import ElRahmaUser
+
+class ElRahmaUserAdmin(UserAdmin):
+    model = ElRahmaUser
+    list_display = ('email', 'phone_number', 'is_staff', 'is_active')
+    list_filter = ( 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('phone_number', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'phone_number', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'phone_number')
+    ordering = ('email',)
+
+admin.site.register(ElRahmaUser, ElRahmaUserAdmin)

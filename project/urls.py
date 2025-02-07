cfe7b0706bce52fd6012b82f3c95c24605381c62
Hesham Moawad <heshammoawad120120@yahoo.com>
2025-02-07 23:40:49 +0200

    :zap: Update .gitignore to exclude media and static files; add products API URLs to project routing

--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.api.urls')),
+    path('api/v1/', include('products.api.urls')),
 
 ]

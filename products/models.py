3a4c66ad20c2683ee0053607340a353ba007aef9
Hesham Moawad <heshammoawad120120@yahoo.com>
2025-02-08 00:07:50 +0200

    :zap: Make image_1 field optional in Product model

--- products/models.py
+++ products/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image_1 = models.ImageField(upload_to='products/')
+    image_1 = models.ImageField(upload_to='products/', blank=True, null=True)
     image_2 = models.ImageField(upload_to='products/', blank=True, null=True)
     image_3 = models.ImageField(upload_to='products/', blank=True, null=True)
     image_4 = models.ImageField(upload_to='products/', blank=True, null=True)

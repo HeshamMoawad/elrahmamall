4f6ebad9db21b792bfe3919ae255fa9dc62e566b
Hesham Moawad <heshammoawad120120@yahoo.com>
2025-02-12 23:21:46 +0200

    :zap: Enhance product search functionality by adding name filter and improving query handling

--- products/views/products.py
+++ products/views/products.py
@@ -1,22 +1,28 @@
+import logging
 from rest_framework.generics import ListAPIView
-from products.models import Product , Category
+from products.models import Product , models
 from products.api.serializers import ProductSerializer
 from rest_framework import filters
+
 
 class ProductsList(ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['categories__name','pk']
+    search_fields = ['categories__name','pk',"name"]
 
     def get_queryset(self):
         queryset = Product.objects.filter(hide=False)
         
+        search_txt = self.request.query_params.get("search",None)
+        if search_txt :
+            return queryset.filter(name__icontains=search_txt)
+        
         product_id = self.request.query_params.get('id', None)
-        if product_id is not None:
+        if product_id :
             return queryset.filter(pk=product_id)
         
         category = self.request.query_params.get('category', None)
-        if category is not None:
+        if category :
             return queryset.filter(categories=category)
         
         return queryset
